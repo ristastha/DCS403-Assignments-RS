4a0876db53faecaa727ed707cac9e8f75c65aa9d
ristastha <stharista78@gmail.com>
2026-01-25 19:41:42 +0545

    Implement sum of first N natural numbers

---
+++ qno1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, sum = 0;
+    cout << "Enter N: ";
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        sum += i;
+    }
+
+    cout << "Sum of first " << N << " natural numbers is: " << sum << endl;
+    return 0;
+}
