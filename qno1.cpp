9a0fcdf9c44a6ca717aba39513c80b21fe3f2bb8
ristastha <stharista78@gmail.com>
2026-01-26 08:27:41 +0545

    Add array printing example in qno1.cpp

---
+++ qno1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arc[] = {10, 20, 30};
+
+    cout << "Elements of array are: ";
+
+    for(int i = 0; i < 3; i++){
+        cout << arc[i] << " ";
+    }
+
+    return 0;
+}
