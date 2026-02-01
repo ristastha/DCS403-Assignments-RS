450b2299de322905bfd65fd0b70c61fa1a689260
ristastha <stharista78@gmail.com>
2026-02-01 22:21:11 +0545

    Add matrix printing functionality in qno6.cpp

---
+++ qno6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printMatrix(int matrix[][4], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int matrix [3][4] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+    printMatrix(matrix, 3, 4);
+
+    return 0;
+}
